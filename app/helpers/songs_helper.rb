26d14c42d41b8a44a4ec602f5564d0d0fa534209
Kirby Campbell <jkirbycampbell@gmail.com>
2018-09-25 01:33:08 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
 
+  def display_song(artist)
+    if artist.song
+      link_to artist.song.title, artist.song
+    else
+      link_to 'Add Song', edit_artist_path(artist)
+    end
+  end
 end
